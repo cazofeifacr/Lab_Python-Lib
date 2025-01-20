52bc63515f68ac0f867da349045e53ad7fe7f105
carlos.azofeifa <carlos.azofeifa@snowflake.com>
2025-01-20 09:30:02 -0600

    Fixed github/scripts/check_readme.py

--- .github/scripts/check_readme.py
+++ .github/scripts/check_readme.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
-import sys
 
 
-def get_changed_files():
+def get_last_commit_hash(file_path):
+    """Get the last commit hash for a given file."""
     result = subprocess.run(
-        ["git", "diff", "--cached", "--name-only"], stdout=subprocess.PIPE
+        ["git", "log", "-n", "1", "--pretty=format:%H", "--", file_path],
+        stdout=subprocess.PIPE,
     )
-    return result.stdout.decode("utf-8").splitlines()
+    return result.stdout.decode("utf-8").strip()
 
 
-def main():
-    changed_files = get_changed_files()
-    readme_file_changed = any("README.file" in file for file in changed_files)
-    readme_md_changed = "README.md" in changed_files
+def find_readme_files():
+    """Find all README.md files in subdirectories."""
+    readme_files = []
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.lower() == "readme.md" and root != ".":
+                readme_files.append(os.path.join(root, file))
+    return readme_files
 
-    if readme_file_changed and not readme_md_changed:
-        print("Error: README.md in the root directory has not been modified.")
-        sys.exit(1)
 
-    sys.exit(0)
+def check_readme_updated():
+    """Check if the root README.md is updated based on other README.md files.
+
+    Raises:
+        Exception: If the root README.md is not updated.
+    """
+    root_readme_last_commit = get_last_commit_hash("README.md")
+    readmes_to_monitor = find_readme_files()
+
+    for readme in readmes_to_monitor:
+        readme_last_commit = get_last_commit_hash(readme)
+        if readme_last_commit > root_readme_last_commit:
+            raise Exception(f"Root README.md is not updated for changes in {readme}")
+
+    print("Root README.md is up to date.")
 
 
 if __name__ == "__main__":
-    main()
+    check_readme_updated()
--- my_hw/README.md
+++ my_hw/README.md
@@ -1,4 +1,4 @@
-# My Project
+# Python library
 
 ## Description
 
