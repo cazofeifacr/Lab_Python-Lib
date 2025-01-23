370c987fa38e044415d80df5aff1f9582bb07ac1
carlos.azofeifa <carlos.azofeifa@snowflake.com>
2025-01-23 09:41:58 -0600

    Set pr_checks temp

--- .github/scripts/pr_check.sh
+++ .github/scripts/pr_check.sh
@@ -5,8 +5,8 @@ HEAD_SHA=$(git rev-parse HEAD)
 CHANGED_PY_FILES=$(git diff --name-only --diff-filter=d "$BASE_SHA" "$HEAD_SHA" | grep '\.py$' | grep -v '^test/' | wc -l)
 
 if [ "$CHANGED_PY_FILES" -gt 6 ]; then
-  echo "PR includes too many Python files (>$CHANGED_PY_FILES). Please split it into smaller PRs."
-  exit 1
+    echo "PR includes too many Python files (>$CHANGED_PY_FILES). Please split it into smaller PRs."
+    exit 1
 else
-  echo "PR size is acceptable."
+    echo "PR size is acceptable."
 fi
