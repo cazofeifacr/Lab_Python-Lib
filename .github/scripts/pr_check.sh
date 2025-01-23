14afef0748b68a914fe2244b6efa2a81c5cb000a
carlos.azofeifa <carlos.azofeifa@snowflake.com>
2025-01-23 09:37:33 -0600

    Set pr_checks temp

--- .github/scripts/pr_check.sh
+++ .github/scripts/pr_check.sh
@@ -2,10 +2,8 @@
 BASE_SHA=$(git merge-base origin/main HEAD)
 HEAD_SHA=$(git rev-parse HEAD)
 
-# Ejecuta el comando para contar los archivos Python cambiados, excluyendo los directorios especificados
-CHANGED_PY_FILES=$(git diff --name-only --diff-filter=d "$BASE_SHA" "$HEAD_SHA" | grep '\.py$' | grep -v '/test/' | wc -l)
+CHANGED_PY_FILES=$(git diff --name-only --diff-filter=d "$BASE_SHA" "$HEAD_SHA" | grep '\.py$' | grep -v '^test/' | wc -l)
 
-# Verifica si el número de archivos Python cambiados es mayor que 6
 if [ "$CHANGED_PY_FILES" -gt 6 ]; then
   echo "PR includes too many Python files (>$CHANGED_PY_FILES). Please split it into smaller PRs."
   exit 1
